7fdbbec5cd72469fe5ae3140414fd3d04c0490de
Khuzaifa Naveed <muhammad.khuzaifa1@gmail.com>
2023-12-17 18:09:10 +0100

    plot Burrau

--- test_energy_error.cpp
+++ test_energy_error.cpp
@@ -288,6 +288,6 @@ void energy_general (std::string in_cond){
 }
 
 int main(){
-    std::string in_cond = "Earth-orbit-w-moon.txt";
+    std::string in_cond = "Burrau.txt";
     energy(in_cond);
 }
